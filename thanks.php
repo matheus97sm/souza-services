a8bd5631dfad56343602445ab8311da96dbcf4ae
matheus97sm <matheus97sm@gmail.com>
2022-01-25 18:30:19 -0300

    New thank you page

---
+++ thanks.php
@@ -0,0 +1,17 @@
+﻿<?php
+/**
+ * Template Name: thanks
+ *
+ * @package WordPress
+ * @subpackage matheusFonseca
+ * @since matheusFonseca
+ */
+get_header(); ?>
+
+<section class="thanks">
+  <div class="container">
+    <p><?=the_content()?></p>
+  </div>
+</section>
+
+<?php get_footer();
